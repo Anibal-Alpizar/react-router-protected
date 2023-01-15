435bd9eadb52aa9b9cb9af6d38349ad46da8e301
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-01-15 14:47:19 -0600

    React Router DOM Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { Admin, Analytics, Dashboard, Home, Landing } from './pages'
+
 function App() {
   return (
-    <div>Hello world</div>
+
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route index element={<Landing />} />
+        <Route path='/landing' element={<Landing />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/analytics' element={<Analytics />} />
+        <Route path='/admin' element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
   )
+}
+
+function Navigation() {
+  return <nav>
+    <ul>
+      <li>
+        <Link to="/landing">Landing</Link>
+      </li>
+      <li>
+        <Link to="/home">Home</Link>
+      </li>
+      <li>
+        <Link to="/dashboard">Dashboard</Link>
+      </li>
+      <Link to="/analytics">Analytics</Link>
+      <li>
+        <Link to="/admin">Admin</Link>
+      </li>
+    </ul >
+  </nav >
 }
 
 export default App
